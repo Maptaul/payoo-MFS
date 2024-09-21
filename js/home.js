f12c32bf2ae8657c4e7927fc4c55019c5e072fe8
@maptaul <maptaulislam1@gmail.com>
2024-09-21 21:16:01 +0600

    send par

--- js/home.js
+++ js/home.js
@@ -1,1 +1,20 @@
-console.log("home added");
+// add money to the account
+// step 1 add event handler
+// prevent page reload after form submit
+
+// step 2 get money to be added to the account balance
+// step 1 add an event handler to the add money button inside the form
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    //   prevent page reload after form submit
+    event.preventDefault();
+    // step 2 get money to be added to the account balance
+
+    const addMoneyInput = document.getElementById("input-add-money").value;
+    console.log(addMoneyInput);
+
+    // step 3 get the pin number provide
+    const pinNumber = document.getElementById("input-pin-number").value;
+    console.log(pinNumber);
+  });
