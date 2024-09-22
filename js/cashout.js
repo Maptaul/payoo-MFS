386be1144166788393ec823a4f15119ee40d8a5f
@maptaul <maptaulislam1@gmail.com>
2024-09-22 10:54:16 +0600

    cashout done

--- js/cashout.js
+++ js/cashout.js
@@ -4,13 +4,16 @@ document
     event.preventDefault();
     console.log("cashout");
     const cashOut = document.getElementById("input-cashout").value;
+    const cashOutNumber = parseFloat(cashOut);
     const pinNumber = document.getElementById("input-cashoutpin").value;
     console.log(cashOut, pinNumber);
+
     if (pinNumber === "1234") {
       const balance = document.getElementById("account-balance").innerText;
-      const balanceNumber = parseFloat(balance);
-      const newBalance = balanceNumber - ca;
+      const balanceNumer = parseFloat(balance);
+      const newBalane = balanceNumer - cashOutNumber;
+      document.getElementById("account-balance").innerText = newBalane;
     } else {
-      alert("failed to cashout");
+      alert("please try agin later");
     }
   });
